4628100baffd11586978ee494d546c4622a0d0ee
leonidSpiri <2003.leonid2003@gmail.com>
2025-02-16 14:02:00 +0300

    update dependencies & gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.4")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.7")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
 
 
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.android.library") version "8.7.3" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("com.android.application") version "8.8.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.android.library") version "8.8.1" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
 }
